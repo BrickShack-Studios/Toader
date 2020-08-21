4ce7321f539ea231a8f82b2bf28f06233e398897
Jordan McIntyre <39545402+Staggier@users.noreply.github.com>
2020-08-20 20:11:18 -0400

    Movement and Library Setup
    Added binary files for SDL2 and SDL2_image libraries.
    
    Added base functionality for toad movement.
    
    Added a SDL_Rect to the Toad struct for its texture's positioning.
    
    Added width and height properties to the Screen struct.
    
    Fixed a typo in the error message for a failed instance of loadTexture.
    
    Restructured some code to allow for events to work properly in main.

--- src/main.c
+++ src/main.c
@@ -14,13 +14,14 @@ typedef struct Screen
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Texture* texture;
+    int width;
+    int height;
 } Screen;
 
 typedef struct Toad
 {
     SDL_Texture* texture;
-    int posX;
-    int posY;
+    SDL_Rect* image;
     int velX;
     int velY;
 } Toad;
@@ -35,6 +36,8 @@ Toad* newToad()
 Screen* init()
 {
     Screen* screen = malloc(sizeof(Screen));
+    screen->width = 224;
+    screen->height = 240;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -43,7 +46,7 @@ Screen* init()
         goto end;
     }
 
-    screen->window = SDL_CreateWindow("Toader", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    screen->window = SDL_CreateWindow("Toader", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen->width, screen->height, SDL_WINDOW_SHOWN);
     if (!screen->window)
     {
         printf("Window could not be created. Error: %s\n", SDL_GetError());
@@ -54,10 +57,10 @@ Screen* init()
     screen->renderer = SDL_CreateRenderer(screen->window, -1, SDL_RENDERER_ACCELERATED);
     if (!screen->renderer)
     {
-	printf("Renderer could not be created. Error: %s\n", SDL_GetError());
-	SDL_DestroyWindow(screen->window);
-	free(screen);
-	goto end;
+        printf("Renderer could not be created. Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(screen->window);
+        free(screen);
+        goto end;
     }
 
     SDL_SetRenderDrawColor(screen->renderer, 0, 0, 0, 255);
@@ -65,10 +68,10 @@ Screen* init()
     int imgFlags = IMG_INIT_PNG;
     if (!(IMG_Init(imgFlags) & imgFlags))
     {
-	printf("SDL_Image could not initialize. Error: %s", IMG_GetError());
-	SDL_DestroyRenderer(screen->renderer);
-	SDL_DestroyWindow(screen->window);
-	free(screen);
+        printf("SDL_Image could not initialize. Error: %s", IMG_GetError());
+        SDL_DestroyRenderer(screen->renderer);
+        SDL_DestroyWindow(screen->window);
+        free(screen);
     }
 
 end:
@@ -82,22 +85,22 @@ SDL_Texture* loadTexture(const char* path, Screen* screen)
     SDL_Surface* surface = IMG_Load(path);
     if (!surface)
     {
-	printf("Error loading %s. Error: %s\n", path, IMG_GetError());
-	goto end;
+        printf("Error loading %s. Error: %s\n", path, IMG_GetError());
+        goto end;
     }
 
     texture = SDL_CreateTextureFromSurface(screen->renderer, surface);
     if (!texture)
     {
-	printf("Error creating texture from %s. Error: %s\n", path, SDL_GetError());
-	goto end;
+        printf("Error creating texture from %s. Error: %s\n", path, SDL_GetError());
+        goto end;
     }
 
 end:
 
     if (surface)
-	SDL_FreeSurface(surface);
-    
+        SDL_FreeSurface(surface);
+
     return texture;
 }
 
@@ -108,9 +111,16 @@ bool loadMedia(Toad* toad, Screen* screen)
     toad->texture = loadTexture("res/sprites/toader/toader.png", screen);
     if (!toad->texture)
     {
-        printf("Unable to load %s. Error: %s\n", "res/sprites/toader/toader.bmp", SDL_GetError());
+        printf("Unable to load %s. Error: %s\n", "res/sprites/toader/toader.png", SDL_GetError());
         success = false;
     }
+
+    toad->image = calloc(1, sizeof(SDL_Rect*));
+
+    toad->image->x = 0;
+    toad->image->y = 0;
+    toad->image->w = 16;
+    toad->image->h = 16;
 
     return success;
 }
@@ -131,14 +141,14 @@ void cleanup(Screen* screen, Toad* toad)
 
     if (screen->renderer)
     {
-	SDL_DestroyRenderer(screen->renderer);
-	screen->renderer = NULL;
+        SDL_DestroyRenderer(screen->renderer);
+        screen->renderer = NULL;
     }
 
     if (screen->texture)
     {
-	SDL_DestroyTexture(screen->texture);
-	screen->texture = NULL;
+        SDL_DestroyTexture(screen->texture);
+        screen->texture = NULL;
     }
 
     free(screen);
@@ -197,6 +207,15 @@ void keyEvents(Toad* toad, SDL_Event e)
     return;
 }
 
+void move(Toad* toad, Screen* screen)
+{
+    if (toad->image->x + toad->velX <= SCREEN_WIDTH && toad->image->x + toad->velX >= 0)
+        toad->image->x += toad->velX;
+    if (toad->image->y + toad->velY <= SCREEN_HEIGHT && toad->image->y + toad->velY >= 0)
+        toad->image->y += toad->velY;
+    return;
+}
+
 int main(int argc, char* argv[])
 {
     Screen* screen = init();
@@ -210,7 +229,7 @@ int main(int argc, char* argv[])
 
     if (!loadMedia(toad, screen))
     {
-	printf("Failed to load media\n");
+        printf("Failed to load media\n");
         goto cleanup;
     }
 
@@ -224,13 +243,15 @@ int main(int argc, char* argv[])
             //User requests quit
             if(e.type == SDL_QUIT)
                 quit = true;
-        }
-	
-        keyEvents(toad, e);
 
-	SDL_RenderClear(screen->renderer);
-	SDL_RenderCopy(screen->renderer, toad->texture, NULL, NULL);
-	SDL_RenderPresent(screen->renderer);
+            keyEvents(toad, e);
+        }
+
+        move(toad, screen);
+
+        SDL_RenderClear(screen->renderer);
+        SDL_RenderCopy(screen->renderer, toad->texture, NULL, toad->image);
+        SDL_RenderPresent(screen->renderer);
     }
 
 cleanup:
